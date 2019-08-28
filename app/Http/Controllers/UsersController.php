1cd4f788c31b0ab1230478e4aef8c2ceb52e3158
vagrant <vagrant@homestead>
2019-08-28 15:31:01 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,12 +10,19 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-           'except' => ['show', 'create' , 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest',[
            'only' => ['create']
         ]);
+    }
+
+    //用户列表页面
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
     //用户注册界面
