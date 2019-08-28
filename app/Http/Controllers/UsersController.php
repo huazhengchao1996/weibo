198cdc46900e2db1ddc0582b1fc62b09b94bd44a
vagrant <vagrant@homestead>
2019-08-28 15:06:26 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,17 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+           'except' => ['show', 'create' , 'store']
+        ]);
+
+        $this->middleware('guest',[
+           'only' => ['create']
+        ]);
+    }
+
     //用户注册界面
     public function create()
     {
@@ -42,12 +53,14 @@ class UsersController extends Controller
     //用户修改界面
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     //用户修改逻辑
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request,[
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
