5add97cdbbe8b32f6ae5c2c90706d0082f8184ce
vagrant <vagrant@homestead>
2019-08-28 14:41:40 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,16 +7,19 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    //用户注册界面
     public function create()
     {
         return view('users.create');
     }
 
+    //用户显示界面
     public function show(User $user)
     {
         return view('users.show',compact('user'));
     }
 
+    //用户注册逻辑
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -34,5 +37,32 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user]);
+    }
+
+    //用户修改界面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //用户修改逻辑
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success','个人资料更新成功！');
+
+        return redirect()->route('users.show',$user);
+
     }
 }
